252bc966ce32bb4e5babbbd422064b8a0563a121
Korben Zhang <korbenzhang@gmail.com>
2015-10-17 17:47:35 +0800

    update

--- mmodel/demo/demo_mmodel/main.go
+++ mmodel/demo/demo_mmodel/main.go
@@ -10,6 +10,10 @@ type Model struct {
 	Name string
 }
 
+func (m Model) TableName() string {
+	return "demo_model"
+}
+
 func Demo(m1 interface{}) {
 	fmt.Println(mmodel.GetModelId(m1))
 	m2 := mmodel.AddModelUuid(m1)
@@ -17,9 +21,20 @@ func Demo(m1 interface{}) {
 	fmt.Printf("%+v\n", m2)
 }
 
-func main() {
+func DemoA() {
 	m1 := &Model{Id: "1", Name: "Demo1"}
 	Demo(m1)
 	m2 := Model{Id: "1", Name: "Demo1"}
 	Demo(m2)
+}
+
+func DemoTableName() {
+	m := Model{}
+	fmt.Printf("%s\n", mmodel.GetModelTableName(m))
+	m2 := &Model{}
+	fmt.Printf("%s\n", mmodel.GetModelTableName(m2))
+}
+
+func main() {
+	DemoTableName()
 }
