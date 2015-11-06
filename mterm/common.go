66f77b894dbcc0ff4e4db7b6444e525b9abd119b
Korben Zhang <korbenzhang@gmail.com>
2015-11-06 17:11:10 +0800

    fix common

--- mterm/common.go
+++ mterm/common.go
@@ -9,7 +9,9 @@ import (
 func IsXterm() bool {
 	t := strings.ToLower(os.Getenv("TERM"))
 	rt := strings.ToLower(os.Getenv("RUN_TERM"))
-	if strings.Contains(t, "xterm") || strings.Contains(t, "linux") || strings.Contains(rt, "xterm") {
+	if strings.Contains(t, "xterm") || 
+		strings.Contains(t, "linux") || 
+		strings.Contains(rt, "xterm") {
 		return true
 	} else {
 		return false
