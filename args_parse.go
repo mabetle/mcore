c3e870a8a179a2f7745557fc100dcb46276fa00b
Korben Zhang <korbenzhang@gmail.com>
2016-03-18 03:39:37 +0100

    add args value func

--- args_parse.go
+++ args_parse.go
@@ -6,7 +6,8 @@ import (
 
 type Args []string
 
-// Parse
+// ParseStringToArgs line to args
+// FIXME not work as expect
 func ParseStringToArgs(s string) []string {
 	r := []string{}
 	s = strings.TrimSpace(s)
@@ -57,4 +58,61 @@ func (a Args) NFlags() (r int) {
 
 func (a Args) VArgs(index int) string {
 	return a[index]
+}
+
+// GetArgString arg format: a=b
+func GetArgString(name string, defaultValue string, args ...string) string {
+	// process Args
+	for _, a := range args {
+		arg := GetString(a)
+		//arg format: a=b
+		arg = strings.TrimSpace(arg)
+		kv := strings.Split(arg, "=")
+		if len(kv) != 2 {
+			continue
+		}
+		key := strings.TrimSpace(kv[0])
+		value := strings.TrimSpace(kv[1])
+		if NewString(key).IsEqualIgnoreCase(name) {
+			return value
+		}
+	}
+	return defaultValue
+}
+
+// GetArgBool
+func GetArgBool(name string, defaultValue bool, args ...string) bool {
+	dVS := "0"
+	if defaultValue {
+		dVS = "1"
+	}
+	bStr := GetArgString(name, dVS, args...)
+	return NewString(bStr).ToBool()
+}
+
+//
+func GetArgInt(name string, defaultValue int, args ...string) int {
+	iStr := GetArgString(name, "", args...)
+	if iStr == "" {
+		return defaultValue
+	}
+	n, err := NewString(iStr).ToInt()
+	if err != nil {
+		return defaultValue
+	}
+	return n
+}
+
+func GetArgExists(name string, args ...string) bool {
+	for _, a := range args {
+		arg := GetString(a)
+		arg = strings.TrimSpace(arg)
+		//arg format: a=b
+		key := strings.Split(arg, "=")[0]
+		key = strings.TrimSpace(key)
+		if NewString(key).IsEqualIgnoreCase(name) {
+			return true
+		}
+	}
+	return false
 }
