5def777e66694e5ce846f25ea8ef204ff6f0fedf
Korben Zhang <korbenzhang@gmail.com>
2016-04-25 01:53:31 -0400

    add mtml func

--- mtml/util.go
+++ mtml/util.go
@@ -5,7 +5,7 @@ import (
 	"html/template"
 )
 
-// MergeText
+// MergeText merge text and data
 func MergeText(text string, data interface{}) (string, error) {
 	t := template.New("merge")
 	t.Parse(text)
@@ -16,7 +16,14 @@ func MergeText(text string, data interface{}) (string, error) {
 	return out.String(), nil
 }
 
-// MergeFile
+// MergeToHtml merge text and data, returns template.HTML
+func MergeToHtml(text string, data interface{}) template.HTML {
+	txt, err := MergeText(text, data)
+	logger.CheckError(err)
+	return template.HTML(txt)
+}
+
+// MergeFile merge files and data to string
 func MergeFile(data interface{}, file ...string) (string, error) {
 	t, err := template.ParseFiles(file...)
 	if err != nil {
