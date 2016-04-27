36a5fcecffcb3357b4ef92dcad9456cc3140ef59
Korben Zhang <korbenzhang@gmail.com>
2016-04-27 00:24:43 -0400

    fix mapp

--- mapp/models.go
+++ mapp/models.go
@@ -3,6 +3,8 @@ package mapp
 import (
 	"fmt"
 	"github.com/mabetle/mcore"
+	"github.com/mabetle/mdb"
+	"strings"
 )
 
 var Models = make(map[string]interface{})
@@ -13,5 +15,32 @@ func RegModels(models ...interface{}) {
 		typ := mcore.GetTypeName(model)
 		key := fmt.Sprintf("%s-%s", pkg, typ)
 		Models[key] = model
+	}
+}
+
+func migrate(xorm *mdb.Xorm, k string, v interface{}) {
+	logger.Info("migrate model:", k)
+	xorm.Migrate(v)
+}
+
+func Migrate(xorm *mdb.Xorm) {
+	for k, v := range Models {
+		migrate(xorm, k, v)
+	}
+}
+
+func MigratePackage(xorm *mdb.Xorm, pkg string) {
+	for k, v := range Models {
+		if strings.HasPrefix(k, pkg) {
+			migrate(xorm, k, v)
+		}
+	}
+}
+
+func MigrateModel(xorm *mdb.Xorm, name string) {
+	for k, v := range Models {
+		if strings.HasSuffix(k, name) {
+			migrate(xorm, k, v)
+		}
 	}
 }
